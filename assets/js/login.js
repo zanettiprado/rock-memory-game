3cdd18740d9553495961b73d1e553f12aed99612
zanettiprado <zanettiprado@gmail.com>
2023-04-21 14:43:38 +0000

    add comments to code in order to make it clear

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,14 @@
+/** 
+ * Declare variables for changing DOM elements
+ * and to get players information.
+ */
 let input = document.querySelector('.login-input');
 let button = document.querySelector('.login-button');
-
+/** 
+ * Function to validate the player's name and check if they are using a valid name.
+ * more than 3 characteres is required;
+ * if they are following the requirements the button play will be able to start the game.
+ */
 function validadeInput(event) {
     let target = event.target;
     if(target.value.length > 2) {
@@ -9,5 +17,7 @@ function validadeInput(event) {
     }
     button.setAttribute('disabled','');
 }
-
+/**
+ * Event lister to hear the function validade input function 
+ */
 input.addEventListener('input', validadeInput);
