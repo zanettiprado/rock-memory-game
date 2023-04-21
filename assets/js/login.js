ec6f89f6a20c62982458bdcc44a25963b232fa0d
zanettiprado <zanettiprado@gmail.com>
2023-04-21 15:26:24 +0000

    add function to save the name, start the game and also add main page

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,22 +2,37 @@
  * Declare variables for changing DOM elements
  * and to get players information.
  */
-let input = document.querySelector('.login-input');
-let button = document.querySelector('.login-button');
+const input = document.querySelector('.login-input');
+const button = document.querySelector('.login-button');
+const form = document.querySelector('.login-form');
+
 /** 
  * Function to validate the player's name and check if they are using a valid name.
  * more than 3 characteres is required;
  * if they are following the requirements the button play will be able to start the game.
  */
 function validadeInput(event) {
-    let target = event.target;
+    const target = event.target;
     if(target.value.length > 2) {
         button.removeAttribute('disabled');
         return;
     }
     button.setAttribute('disabled','');
 }
+/** 
+ * Function to handle the player information 
+ * save the player name and send to display
+ * and to call the main page of the game.
+*/
+function handleSubmit(event) {
+    event.preventDefault();
+    localStorage.setItem('player', input.value);
+    window.location = 'game.html'
+
+
+}
 /**
- * Event lister to hear the function validade input function 
+ * Event listener to hear the function validade input function 
  */
 input.addEventListener('input', validadeInput);
+form.addEventListener('submit', handleSubmit);
