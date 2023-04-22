58de3b7a3a318e085fed359278a5614b06f502d2
zanettiprado <zanettiprado@gmail.com>
2023-04-22 16:07:26 +0000

    last commit was not saved. Few changes in css flip card and create array and loadGame function to create 10 differents cards but is not working at the moment

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,46 @@
 const grid = document.querySelector('.grid');
 
+const singers = [
+    'amy',
+    'bono',
+    'dave',
+    'elvis',
+    'eric',
+    'freedy',
+    'kurt',
+    'michael',
+    'slash',
+    'tina',
+];
+
 const createElement = (tag, className) => {
     const element = document.createElement(tag);
     element.className = className;
     return element;
 }
 
-const createCard = () => {
+const createCard = (singer) => {
 
     const card = createElement('div', 'card');
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
+    front.style.backgroundImage = `url('../images/${singer}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
 
-    grid.appendChild(card);
+    return card;
 
 }
 
-createCard();
+const laodGame = () => {
+
+    singers.forEach((singer) => {
+
+        const card = createCard(singer);
+
+        grid.appendChild(card);
+    });
+}
+laodGame();
