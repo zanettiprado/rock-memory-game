3b9323c9f145392afcb9f2680c964325f76b706e
zanettiprado <zanettiprado@gmail.com>
2023-04-22 12:19:35 +0000

    fix the create element card now is working

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,17 @@
 const grid = document.querySelector('.grid');
 
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
 const createCard = () => {
 
-    const card = document.createElement('div');
-    const front = document.createElement('div');
-    const back = document.createElement('div');
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
 
-    card.className = 'card';
-    front.className = 'face front';
-    back.className = 'face back';
 
     card.appendChild(front);
     card.appendChild(back);
