ee3ca1ce84bbccfcdc97b8f063509fee5e1671b5
zanettiprado <zanettiprado@gmail.com>
2023-04-22 12:13:40 +0000

    create card teste and change again to keep it in track

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,20 @@
 const grid = document.querySelector('.grid');
 
-const createElement = (tag, className) => {
-    const element = document.createElement(tag);
-    element.className = className;
-    return element;
-}
-
 const createCard = () => {
 
-    const card = document.createElement('div', 'card');
-    const front = document.createElement('div', 'face front');
-    const back = document.createElement('div', 'face back');
+    const card = document.createElement('div');
+    const front = document.createElement('div');
+    const back = document.createElement('div');
+
+    card.className = 'card';
+    front.className = 'face front';
+    back.className = 'face back';
 
     card.appendChild(front);
     card.appendChild(back);
 
-    return card;
+    grid.appendChild(card);
 
 }
 
+createCard();
