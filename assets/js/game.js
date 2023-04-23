a7af6b1def4fa908e55876292d5d5df5b8afc98b
zanettiprado <zanettiprado@gmail.com>
2023-04-23 12:23:44 +0000

    delete flip rotate and add function to turn cards when clicked just twice

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,6 +14,8 @@ const singers = [
     'slash',
     'tina',
 ];
+
+
 /** 
  * const to create elements with the same classe we have created before in HTML document 
  * from now elements will be created here in js matching this create element and create card const
@@ -23,9 +25,34 @@ const createElement = (tag, className) => {
     element.className = className;
     return element;
 }
+
+let firstCard = '';
+let secondCard = '';
+
+
+const turnCard = ({ target }) => {
+
+    if (target.parentNode.className.includes('turn-card')) {
+        return;
+    }
+
+    if (firstCard === '') {
+        target.parentNode.classList.add('turn-card');
+        firstCard = target.parentNode; 
+    } else if(secondCard === '') {
+        target.parentNode.classList.add('turn-card');
+        secondCard = target.parentNode;
+    }
+
+
+    
+
+}
+
 /**
  * const to create card using the elements above. It will take the elements, match with
  * each class and get information from our arrays and bring the images from the folder. 
+ * addEventlistner that matchs with the function to reveal the card
  */
 const createCard = (singer) => {
 
@@ -38,15 +65,22 @@ const createCard = (singer) => {
     card.appendChild(front);
     card.appendChild(back);
 
+    card.addEventListener('click', turnCard);
+
     return card;
 }
 /**
  * function to load the game 
  * It will create the 10 cards with each singer as per our array name.
+ * duplicate const was overposted to create 2x our first array.
+ * Method sort + math.random was used to shuffle the cards in the array.
  */
 const laodGame = () => {
 
-    const duplicateSingers = [ ...singers, ...singers]
+    const duplicateSingers = [...singers, ...singers];
+
+    const shuffleCards = duplicateSingers.sort(() => Math.random() - .5);
+
     duplicateSingers.forEach((singer) => {
 
         const card = createCard(singer);
